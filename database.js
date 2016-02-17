612eb2f858aa99c6a9206c5ac050d6ec29416dea
Mark Crisp <macr1324@gmail.com>
2016-02-17 18:09:19 -0500

    fixed conflicts

--- database.js
+++ database.js
@@ -54,11 +54,7 @@ var dbInit = db.query('create database reddit_clone')
 	User.hasMany(Session);
 	Session.belongsTo(User);
 
-<<<<<<< HEAD
-	var Content = db.define('content', {
-=======
 	Content = db.define('content', {
->>>>>>> database
 	  url: Sequelize.STRING,
 	  title: Sequelize.STRING
 	});
@@ -101,8 +97,6 @@ function login(username, password) {
 			})
 			.then(function(session) {
 				return session.dataValues.token;
-<<<<<<< HEAD
-=======
 			});
 		});
 	});
@@ -133,44 +127,13 @@ function createNewContent(sessionId, url, title) {
 			.then(function(content) {
 		    user.addContent(content);
 		    return content;
->>>>>>> database
 			});
 		});
 	});
 }
-<<<<<<< HEAD
-//Given a sessionId, return
-function getUserFromSessionId(sessionId) {
-	return Session.findOne({
-		include:[User],
-		where: {
-			token: sessionId
-		}
-	});
-}
-/* takes a sessionId, a URL, a title, creates a new content with Sequelize and returns the content in a promise
-*/
-function createNewContent(sessionId, url, title) {
-  return dbInit.then(function() {
-		return User.findById(userId)
-	  .then(function(user) {
-	    return Content.create({
-	      url: url,
-	      title: title
-	    })
-	    .then(function(content) {
-	      user.addContent(content);
-	      return content;
-	    });
-	  });
-	});
-}
 
 getUserFromSessionId('4ad8517348b7f054683a73d046448f5bc337825eb9afac4b5179af87bf21143d854c8bf2585f7f4e').then(console.log);
-=======
 
-
->>>>>>> database
 module.exports = {
 	createNewUser: createNewUser,
 	login: login,
