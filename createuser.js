d3d7045651422ae16465b8b8b9b69c93ffe7661e
andyding <andy.ding89@gmail.com>
2016-02-17 20:33:06 +0000

    listening now

--- createuser.js
+++ createuser.js
@@ -5,6 +5,7 @@ var bodyParser = require('body-parser');
 var database = require('database.js');
 
 var parser = bodyParser.urlencoded({ extended: false });
+
 app.post('/SignUp', parser, function(request, response){
     if(request.body.password === request.body.confirmPassword){
         database.createNewUser(request.body.user, request.body.password, request.body.email)
@@ -15,4 +16,11 @@ app.post('/SignUp', parser, function(request, response){
     else{
         response.send('<h1>Your passwords do not match. Please try again.</h1>');
     }
+});
+
+var server = app.listen(process.env.PORT, process.env.IP, function () {
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log('Example app listening at http://%s:%s', host, port);
 });
