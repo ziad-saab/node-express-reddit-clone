58b48691804b0ae7d59129ec13b110bf7392cb90
Mark Crisp <macr1324@gmail.com>
2016-02-17 15:00:06 -0500

    added some comments

--- database.js
+++ database.js
@@ -8,7 +8,9 @@ db.configure({
 	"password": "",
 	"database": ""
 });
+
 var User;
+
 //create database if it doesn't already exist
 var dbInit = db.query('create database reddit_clone')
 .catch(function(e) {
@@ -18,17 +20,21 @@ var dbInit = db.query('create database reddit_clone')
 	else throw e;
 })
 .then(function(res) {
+	//change databae to sequelize after creating a database
 	db = new Sequelize('reddit_clone', username, '', {
 		dialect: 'mysql'
 	});
+	//define users table
 	User = db.define('user', {
 	    username: {type: Sequelize.STRING(20), unique: true, allowNull: false},
 	    password: {type: Sequelize.STRING(20), allowNull: false}, // TODO: make the passwords more secure!
 			email: Sequelize.STRING(50)
 	});
 	return db.sync();
+	//return;
 })
 
+//Inserts a new user into users table
 function createNewUser(username, password, email) {
   return dbInit.then(function(res) {
 		return User.create({
