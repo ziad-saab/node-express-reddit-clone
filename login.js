ce6bb5f0412ef5452416371b0b04861d999ed23d
andyding <andy.ding89@gmail.com>
2016-02-17 21:44:32 +0000

    added login user html and js file

---
+++ login.js
@@ -0,0 +1,31 @@
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var database = require('./database.js');
+var config = require('./config.json');
+var cookieParser = require('cookie-parser');
+app.use(cookieParser());
+
+var parser = bodyParser.urlencoded({ extended: false });
+
+app.get('/Login', function(req, res){
+  res.sendFile('/Login/index.html', {root: __dirname });
+});
+
+app.post('/Login', parser, function(request, response){
+    database.login(request.body.username, request.body.password).
+    then(function(token){
+        response.send("You are now logged in with token: " + token);
+        //need to add code to use the token for the login session
+    }).catch(e){
+        response.send(e);
+        throw e;
+    };
+});
+
+var server = app.listen(process.env.PORT, process.env.IP, function () {
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log('Example app listening at http://%s:%s', host, port);
+});
