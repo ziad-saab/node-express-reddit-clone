6c2034d35a07caa7f8ef3eac3014c7235dec83ad
Mark Crisp <macr1324@gmail.com>
2016-02-17 15:48:37 -0500

    finished user creation

--- createuser.js
+++ createuser.js
@@ -3,27 +3,35 @@ var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 var database = require('./database.js');
+var config = require('./config.json');
 
 var parser = bodyParser.urlencoded({ extended: false });
 
+app.get('/SignUp', function(req, res){
+  res.sendFile('/home/mark/Programming Docs/github/node-express-sequelize-reddit-clone/SignUp/index.html');
+});
+
 app.post('/SignUp', parser, function(request, response){
     if(request.body.password === request.body.confirmPassword){
-        database.createNewUser(request.body.user, request.body.password, request.body.email)
+        database.createNewUser(request.body.username, request.body.password, request.body.email)
         .then(function(result){
             response.send('<h1>Nice Job!</h1>');
+        })
+        .catch(function(e) {
+          if(e.name === 'SequelizeUniqueConstraintError') {
+            response.send('<h1>Username taken</h1>');
+          }
+          else throw e;
         });
     }
     else{
         response.send('<h1>Your passwords do not match. Please try again.</h1>');
     }
 });
-<<<<<<< HEAD
-=======
 
-var server = app.listen(process.env.PORT, process.env.IP, function () {
+var server = app.listen(config.port, config.server, function () {
   var host = server.address().address;
   var port = server.address().port;
 
   console.log('Example app listening at http://%s:%s', host, port);
 });
->>>>>>> d3d7045651422ae16465b8b8b9b69c93ffe7661e
