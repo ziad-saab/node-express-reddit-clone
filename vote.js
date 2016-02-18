71bede8db031dc381038739572640a25e79efd8a
Mark Crisp <macr1324@gmail.com>
2016-02-18 18:40:13 -0500

    added upvote colour update and removed bullet points

--- vote.js
+++ vote.js
@@ -2,8 +2,12 @@ var app = require('./app.js');
 var database = require('./database.js');
 
 app.get('/upvote/:contentId', function(req, res){
-  database.voteOnContent(req.cookies.sessionId, req.params.contentId, true);
+  database.voteOnContent(req.cookies.sessionId, req.params.contentId, true).then(function(resp) {
+    res.redirect('/');
+  });
 });
 app.get('/downvote/:contentId', function(req, res){
-  database.voteOnContent(req.cookies.sessionId, req.params.contentId, false);
+  database.voteOnContent(req.cookies.sessionId, req.params.contentId, false).then(function(resp) {
+    res.redirect('/');
+  });
 });
