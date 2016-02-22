39ba92dbb0a0a723493ea679062582f05f92ac4f
Mark Crisp <macr1324@gmail.com>
2016-02-22 11:39:54 -0500

    fixed voting redirect

--- vote.js
+++ vote.js
@@ -1,13 +1,13 @@
 var app = require('./app.js');
 var database = require('./database/database.js');
 
-app.post('/upvote/:order/:contentId', function(req, res){
+app.post('/upvote/:contentId', function(req, res){
   database.voteOnContent(req.cookies.sessionId, req.params.contentId, true).then(function(resp) {
-    res.redirect('/'+req.params.order);
+    res.redirect(req.get('referer'));
   });
 });
-app.post('/downvote/:order/:contentId', function(req, res){
+app.post('/downvote/:contentId', function(req, res){
   database.voteOnContent(req.cookies.sessionId, req.params.contentId, false).then(function(resp) {
-    res.redirect('/'+req.params.order);
+    res.redirect(req.get('referer'));
   });
 });
