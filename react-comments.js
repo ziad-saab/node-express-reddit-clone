7904f79e5aa66e4b570ef0b2d27eedbd3647bce4
Mark Crisp <macr1324@gmail.com>
2016-02-22 16:01:15 -0500

    fixed react-comments

--- react-comments.js
+++ react-comments.js
@@ -1,31 +1,35 @@
+var React = require('react');
+
 function renderComment(comment, n) {
     var upvote = "/tinygreyupvote.ico";
     var downvote = "/tinygreydownvote.ico";
     var margins = "margin-left: 40px;";
-    
+    var comments;
     if (comment.children) {
-        var comments = comment.children.forEach(function(child) {
-          renderComment(child, n + 10);
+        comments = comment.children.map(function(child) {
+          return renderComment(child, n + 10);
         });
     }
-    
+
     return (
-      <div class="commentDepth">
-      <li style="list-style: none;" class="content-item">
+      <div className="commentDepth" key={comment.id}>
+      <li style={{"listStyle": "none"}} className="content-item">
         <hr></hr>
         <table>
+        <tbody>
           <tr>
             <td><a><img src={upvote}></img></a></td>
-            <td><p class="createdby">Post by <span class="usercreater">{comment.user.username}</span></p></td>
+            <td><p className="createdby">Post by <span className="usercreater">{comment.user.username}</span></p></td>
           </tr>
           <tr>
-            <td><p class="votescore"></p></td>
-            <td><a class="contentpost">{comment.text}</a></td>
+            <td><p className="votescore"></p></td>
+            <td><a className="contentpost">{comment.text}</a></td>
           </tr>
           <tr>
             <td><a><img src={downvote}></img></a></td>
-            <td><a class="comments" href="">Direct Link</a></td>
+            <td><a className="comments" href="">Direct Link</a></td>
           </tr>
+          </tbody>
         </table>
       </li>
       <hr></hr>
@@ -39,52 +43,52 @@ function Comments(user, content, comments){
     if (user)
     header = (
     <div>
-      <h4 id="welcomeuser">Welcome <span id="user">{user}</span></h4>
+      <h4 id="welcomeuser">Welcome <span id="user">{user.username}</span></h4>
       <a href="/Logout" className="link">Logout</a>
       <a href="/CreateContent" className="link">Create Content</a>
     </div>);
-    
+
     else header = (
     <div>
       <a href="/SignUp" className="link">Sign Up</a>
       <a href="/Login" className="link">Login</a>
     </div>
     );
-    
+
     var commentAction = "/comment/" + content.id;
-    var comments = comments.forEach(renderComment, 0);
-    
+    var comments = comments.map(renderComment);
+
     return (
         <html>
             <head>
-                <meta charset="utf-8"/>
+                <meta charSet="utf-8"/>
                 <link href="/comment.css" rel="stylesheet" type="text/css"/>
             </head>
             <body>
-              
+
               <nav>
                 {header}
               </nav>
-              
+
               <main>
               <div id="contents">
                 <a id="heading" href={content.url}>{content.title}</a>
-                <ul class="contents-list">
+                <ul className="contents-list">
                   <span id="contentList">
                     <form action={commentAction} method="post">
-                      <label for="comment"><span>Comment:</span><input type="text" class="input-field" name="comment" value="" maxlength="255"/></label>
+                      <label htmlFor="comment"><span>Comment:</span><input type="text" className="input-field" name="comment" value="" maxLength="255"/></label>
                       <label><span>&nbsp;</span><input type="submit" value="Reply" /></label>
                     </form>
-                    
+
                     <article>
                     {comments}
                     </article>
-                    
+
                   </span>
                 </ul>
               </div>
               </main>
-              
+
             </body>
         </html>
     );
