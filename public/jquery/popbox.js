1447dc37294820ea0ec231278e960cf463302680
andyding1 <andy.ding89@gmail.com>
2016-02-24 15:54:12 -0500

    modified popboxes to include color theme and center on page

--- public/jquery/popbox.js
+++ public/jquery/popbox.js
@@ -2,13 +2,21 @@ $(document).ready(function() {
     $('a.signup').click(function() {
         //hide loginbox if it has been clicked
         $(".loginbox").hide();
+
+        var placement = $("body").width()/2 - $(".signupbox").width()/2;
+        $(".signupbox").css("left", placement);
         //Fade in the Popup
         $(".signupbox").fadeIn(300);
+
     });
     $('a.login').click(function() {
         //hide loginbox if it has been clicked
         $(".signupbox").hide();
+
+        var placement = $("body").width()/2 - $(".loginbox").width()/2;
+        $(".loginbox").css("left", placement);
         //Fade in the Popup
         $(".loginbox").fadeIn(300);
+        
     });
 });
