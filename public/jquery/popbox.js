083827d2371acc7e864028f58f12c12f59f6784e
Mark Crisp <macr1324@gmail.com>
2016-02-25 14:21:26 -0500

    fixed mask click listener

--- public/jquery/popbox.js
+++ public/jquery/popbox.js
@@ -1,34 +1,40 @@
 $(document).ready(function() {
-    $('a.signup').click(function() {
-        //hide loginbox if it has been clicked
-        $(".loginbox").hide();
+  $('a.signup').click(function() {
+    //hide loginbox if it has been clicked
+    $(".loginbox").hide();
 
-        var placement = $("body").width()/2 - $(".signupbox").width()/2;
-        $(".signupbox").css("left", placement);
-        //Fade in the Popup
-        $(".signupbox").fadeIn(300);
+    var placement = $("body").width()/2 - $(".signupbox").width()/2;
+    $(".signupbox").css("left", placement);
+    //Fade in the Popup
+    $(".signupbox").fadeIn(300);
 
-        //apply the mask which will grey out background
-        $('body').append('<div id="mask"></div>');
-        $('#mask').fadeIn(300);
-    });
-    $('a.login').click(function() {
-        //hide loginbox if it has been clicked
-        $(".signupbox").hide();
+    //apply the mask which will grey out background
+    $('body').append('<div id="mask"></div>');
+    $('#mask').unbind();
+    $('#mask').on("click", fadeOut);
+    $('#mask').fadeIn(300);
+  });
+  $('a.login').click(function() {
+    //hide loginbox if it has been clicked
+    $(".signupbox").hide();
 
-        var placement = $("body").width()/2 - $(".loginbox").width()/2;
-        $(".loginbox").css("left", placement);
-        //Fade in the Popup
-        $(".loginbox").fadeIn(300);
+    var placement = $("body").width()/2 - $(".loginbox").width()/2;
+    $(".loginbox").css("left", placement);
+    //Fade in the Popup
+    $(".loginbox").fadeIn(300);
 
-        //apply the mask which will grey out background
-        $('body').append('<div id="mask"></div>');
-        $('#mask').fadeIn(300);
-    });
+    //apply the mask which will grey out background
+    $('body').append('<div id="mask"></div>');
+    $('#mask').unbind();
+    $('#mask').on("click", fadeOut);
+    $('#mask').fadeIn(300);
+  });
 
-    $('a.close, #mask').click(function() {
-        $('#mask , .signupbox, .loginbox').fadeOut(300 , function() {
-          $('#mask').remove();
-        });
-    });
+  $('a.close').on("click", fadeOut);
 });
+
+function fadeOut() {
+  $('#mask , .signupbox, .loginbox').fadeOut(300)
+  $('#mask').remove();
+  console.log('clicked');
+}
