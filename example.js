fa1f8093699bfceafe98de6d9c4b152757af82bf
Ziad Saab <ziad.saab@gmail.com>
2016-07-26 16:01:45 -0400

    Update example.js

--- example.js
+++ example.js
@@ -4,7 +4,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var cookieParser = require('cookie-parser');
-var Sequelize = require('sequelize');
 
 // Middleware
 app.use(bodyParser.urlencoded());
