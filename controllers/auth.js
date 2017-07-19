fb1d8d279df762bf092da2812f398f59dd0bf299
Alex Viapiano <alexviapiano@hotmail.com>
2017-07-19 22:13:14 +0000

    Implemented cookies

--- controllers/auth.js
+++ controllers/auth.js
@@ -14,17 +14,23 @@ module.exports = function(myReddit) {
     
     authController.post('/login', function(request, response) {
         
-        //response.send("Attempting login -> username: " + request.body.username + ", password: " + request.body.password);
+        console.log("Attempting login -> username: " + request.body.username + ", password: " + request.body.password);
 
         myReddit.checkUserLogin({
             username: request.body.username,
             password: request.body.password
         })
         
-        // .then(result => {
-        //     myReddit.createUser(result.id)
-        // })
-        
+        .then(result => {
+            console.log("Login User#:")
+            console.log(result.id);
+            myReddit.createUserSession(result.id)
+            .then(session => {
+                response.cookie("SESSION", session.token);
+                response.redirect('/');
+                return;
+            });
+        });
     });
     
     
