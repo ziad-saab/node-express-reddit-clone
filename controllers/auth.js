a9eb249bf7ea8da9bd907cb81f01b910e51ecb07
Alex Viapiano <alexviapiano@hotmail.com>
2017-07-19 00:20:12 +0000

    checkUserLogin() completed

--- controllers/auth.js
+++ controllers/auth.js
@@ -14,13 +14,16 @@ module.exports = function(myReddit) {
     
     authController.post('/login', function(request, response) {
         
-        response.send("Attempted login -> username: " + request.body.username + ", password: " + request.body.password);
-        
-        console.log("Attempted login -> username: " + request.body.username + ", password: " + request.body.password);
-        
+        //response.send("Attempting login -> username: " + request.body.username + ", password: " + request.body.password);
+
         myReddit.checkUserLogin({
-            username: request.body.username
+            username: request.body.username,
+            password: request.body.password
         })
+        
+        // .then(result => {
+        //     myReddit.createUser(result.id)
+        // })
         
     });
     
