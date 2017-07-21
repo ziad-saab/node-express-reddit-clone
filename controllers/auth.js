5f86d41217a790599195667a0c9565baf5bec568
Matt Szabo <mattidj@gmail.com>
2017-07-21 15:44:57 +0000

    saving to git

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,22 +1,42 @@
 var express = require('express');
+//var SESSION;
 
 module.exports = function(myReddit) {
     var authController = express.Router();
     
     authController.get('/login', function(request, response) {
-        response.send("TO BE IMPLEMENTED");
+       response.render('login-form.pug');
     });
     
     authController.post('/login', function(request, response) {
-        response.send("TO BE IMPLEMENTED");
+       
+        myReddit.checkUserLogin(request.body.username, request.body.password)
+        .then(result => myReddit.createUserSession(result))
+        .then(token => {
+               // console.log("the token before sending", token)
+                response.cookie('SESSION', token)
+                response.redirect('/'); 
+        }) .catch(error => {
+             response.status(401).send('Unauthorized Access');
+        });
+        
     });
-    
+
     authController.get('/signup', function(request, response) {
-        response.send("TO BE IMPLEMENTED");
+       response.render('signup-form.pug');
     });
     
     authController.post('/signup', function(request, response) {
-        response.send("TO BE IMPLEMENTED");
+      if (!request.body) return response.sendStatus(400)
+
+    myReddit.createUser({
+            username: request.body.username,
+            password: request.body.password
+          
+        });
+        
+        response.redirect('https://redditnodejs-mattszabo.c9users.io/auth/login');
+
     });
     
     return authController;
